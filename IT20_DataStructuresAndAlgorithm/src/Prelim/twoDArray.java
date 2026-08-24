f14a9136656f101593bf25bb0b275bac8f7f19c7
cliffamadeus <dev.clfm5t12@gmail.com>
2026-08-24 12:07:47 +0800

    Added 2D array code

---
+++ IT20_DataStructuresAndAlgorithm/src/Prelim/twoDArray.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author admin
+ */
+public class twoDArray {
+
+    public static void main(String[] args) {
+
+        int grades[][] = {
+            {70, 75, 71},
+            {75, 72, 76},
+            {79, 78, 90}
+        };
+
+//          Print specific element
+        System.out.println(grades[0][1]);
+    }
+
+}
