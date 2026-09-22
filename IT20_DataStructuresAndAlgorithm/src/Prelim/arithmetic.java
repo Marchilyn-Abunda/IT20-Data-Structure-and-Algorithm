1fdc77e71f560c2eee4e6eb89682031fcea861ba
Marchilyn A. Abunda <marchilynabunda@gmail.com>
2026-09-22 15:40:42 +0800

    Modified arithmetic

--- IT20_DataStructuresAndAlgorithm/src/Prelim/arithmetic.java
+++ IT20_DataStructuresAndAlgorithm/src/Prelim/arithmetic.java
@@ -6,3 +6,9 @@ public class arithmetic {
     
     
 }
+
+
+select t2.event_name as "Name of Event",
+t2.category as "Event Category", 
+ t1.fullname as "Name of Facilitator" 
+ from tbl_facilitator t1 inner join tbl_event t2 on t1.assigned_event_id = t2.event_id;
